56ff427d1dd65993ccd24f62dbfe52053ebe32de
Maxim <81485379+MMLand@users.noreply.github.com>
2025-05-27 21:39:00 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
 function showPdf(modelName) {
   const pdfViewer = document.getElementById('pdf-viewer');
-  pdfViewer.src = `resources/${modelName}.pdf`;
+  pdfViewer.src = `/resources/${modelName}.pdf`;
 }
